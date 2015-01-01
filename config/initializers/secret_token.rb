f14ed79d1637c6d39a9aec709c7f96fb6c07fc15
Mehmet Kemal Bayer <kemal.bayer@gmail.com>
2015-01-01 09:20:24 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PartyJungleWithOmniAuth::Application.config.secret_key_base = '9e8b6ba0a0420e1d8bf1fd141f276323974706f3be2334847ff9ef692f12010a547263b1bab580ab00bca0923dee94400cfb65e3cc067063ea30882a2297793a'
